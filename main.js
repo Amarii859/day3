7967d222bbeb6e4e8f2d64ef51fa2ef1383b3441
Amarii859 <amarbuzoku123@gmail.com>
2025-01-16 19:12:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var firstName = "Digital";
+var lastName = "School"
+var fullname;
+
+console.log(firstName + " " + lastName)
+
+var school = "Digital School"
+console.log(`hello from ${school}`)
+
+var age = 18
+console.log(age>=18 && age <=65)
+
+var var1 = 5
+var var2 = 6
+console.log(var1 == 5 || var2 == 5)
+
+
+var var3 = 9
+var var4 = 10
+console.log(!(var3==var4))
+
+
+function showMessage(){
+    alert("HELLOOOOOO!!!!")
+}
+
+showMessage();
+
+
+function sum(x,y){
+    return x+y
+}
+console.log(sum(6,7))
+
+
+function challage(f){
+    return((f-32)*5/9)
+}
+console.log(challage(50))
